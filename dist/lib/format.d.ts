369df78dd53b8d3e7da87db82dcb6c10c87f63a0
Octagon <ugorji757@gmail.com>
2026-07-06 00:23:44 +0100

    removed dist folder

--- dist/lib/format.d.ts
+++
@@ -1,7 +0,0 @@
-import { type DetectedEnvironment } from './detector.js';
-export interface FormatOptions {
-    enabled?: boolean;
-    env?: DetectedEnvironment;
-}
-export declare function formatFile(absPath: string, cwd: string, opts?: FormatOptions): Promise<void>;
-//# sourceMappingURL=format.d.ts.map
